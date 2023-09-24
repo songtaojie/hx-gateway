cfb03d7368e90fff796a95fb9176dad8de1df096
songtaojie <stjworkemail@163.com>
2023-09-24 20:26:45 +0800

    update

---
+++ Hx.Gateway.Admin/Pages/GlobalConfiguration/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hx.Gateway.Admin.Pages.GlobalConfiguration
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
